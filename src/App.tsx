f562abd416f524980b0fb2776f6bf96ed779fb6f
Bruno Arruda <bruno.arrm@gmail.com>
2023-07-10 21:57:19 -0300

    Comunicação com API estabelecida.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router-dom"
-
+import classes from './App.module.css';
 
 function App() {
 
   return (
-    <div className='app'>
+    <div className={classes.app}>
       <h1>Finder GitHub</h1>
       <Outlet />
     </div>
--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,14 +1,28 @@
+type SearchProps = {
+    loadUser: (userName: string) => Promise<void>
+}
+
+import { useState } from 'react';
+
 import {BsSearch} from 'react-icons/bs';
+import classes from './Search.module.css'
 
 
-const Search = () => {
+const Search = ({loadUser}: SearchProps) => {
+    const [userName, setUserName] = useState('');
+
+
   return (
-    <div className='search'>
+    <div className={classes.search}>
       <h2>Busque por um usuário</h2>
       <p>Conheça seus melhores repositórios</p>
-      <div>
-        <input type='text' placeholder='Digite o nome do usuário' />
-        <button>
+      <div className={classes.search_container}>
+        <input 
+            type='text' 
+            placeholder='Digite o nome do usuário'
+            onChange={(e) => setUserName(e.target.value)} 
+        />
+        <button onClick={() => loadUser(userName)}>
             <BsSearch />
         </button>
       </div>
