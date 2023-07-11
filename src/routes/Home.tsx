481587ec193cce4365a47ae76696e8cd00a48ce0
Bruno Arruda <bruno.arrm@gmail.com>
2023-07-11 14:02:51 -0300

    Criado componente de detalhes do usuario

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,6 +1,7 @@
 import { UserProps } from '../types/user';
 import Search from '../components/Search/Search';
 import { useState } from 'react';
+import User from '../components/User/User';
 
 const Home = () => {
     const [user, setUser] = useState<UserProps | null>(null);
@@ -26,7 +27,7 @@ const Home = () => {
   return (
     <div className='home'>
       <Search loadUser={loadUser} />
-      {user && <p>{user.login}</p>}
+      {user && <User />}
     </div>
   );
 }
