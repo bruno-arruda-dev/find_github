f8a382740e8ea0e5ac9fa4cb7052f79680e7e11a
Bruno Arruda <bruno.arrm@gmail.com>
2023-07-17 23:15:46 -0300

    Configurações de deploy.

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Router>
+    <Router basename='/find_github'>
       <App />
     </Router>
   </React.StrictMode>,
