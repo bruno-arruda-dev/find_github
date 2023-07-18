1b8dff110ca92074a557ecb136e9477dbdad1538
Bruno Arruda <bruno.arrm@gmail.com>
2023-07-17 22:12:16 -0300

    troca do favicon

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,7 @@ import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Router basename='/find_github'>
-    {/* <Router> */}
+    <Router>
       <App />
     </Router>
   </React.StrictMode>,
